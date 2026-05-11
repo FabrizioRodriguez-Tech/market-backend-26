08f3bcc5746d7cbe4b280c9021b447d1079c9a7f
FabrizioRodriguez-Tech <alexander.rodriguez@tecdesoftware.edu.mx>
2026-05-11 08:25:57 -0600

    chore: initial Sprint Boot project setup

---
+++ src/test/java/mx/edu/tecdesoftware/market_backend_26/MarketBackend26ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackend26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
